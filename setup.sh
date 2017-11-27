a270472095280dcfd185f5f4234151b45c238d30
farisj <jakefaris@outlook.com>
2017-11-27 11:52:36 -0500

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Link .bash_aliases
+ln -nfs ~/dotfiles/lib/bash_aliases ~/.bash_aliases
+# Link .bash_profile
+ln -nfs ~/dotfiles/lib/bash_profile ~/.bash_profile
+# Link .git-completion.bash
+ln -nfs ~/dotfiles/vendor/git-completion.bash ~/.git-completion.bash
