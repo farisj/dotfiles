73f1955704ad2343f4a15af64862416f556983d7
farisj <jakefaris@outlook.com>
2017-11-29 12:04:09 -0500

    Add gitignore

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ ln -nfs ~/dotfiles/lib/bash_aliases ~/.bash_aliases
 ln -nfs ~/dotfiles/lib/bash_profile ~/.bash_profile
 # Link .git-completion.bash
 ln -nfs ~/dotfiles/vendor/git-completion.bash ~/.git-completion.bash
+# Link .gitignore
+ln -nfs ~/dotfiles/lib/gitignore ~/.gitignore
+# Add global .gitignore as global core.excludesfile
+git config --global core.excludesfile ~/.gitignore
