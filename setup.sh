5e9af4add347b5b806a77010f76edd5a5a6fd8e0
farisj <jake@aircall.io>
2019-07-30 10:44:56 -0400

    Update Dotfiles
    Add aliases for docker and git
    Add brew and powerline to setup.sh
    Other minor updates

--- setup.sh
+++ setup.sh
@@ -18,9 +18,20 @@ ln -nfs ~/dotfiles/lib/vimrc ~/.vimrc
 ln -nfs ~/dotfiles/lib/.tmux.conf ~/.tmux.conf
 # Setup Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install Powerline Fonts
+git clone git@github.com:powerline/fonts.git
+./vendor/fonts/install.sh
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "You are all set up!"
 echo "Consider downloading the following products:"
 
 echo ""
 echo "SizeUp: http://www.irradiatedsoftware.com/sizeup/"
+
+echo ""
+echo "Iterm jump left/right: https://coderwall.com/p/h6yfda/use-and-to-jump-forwards-backwards-words-in-iterm-2-on-os-x"
+
+echo ""
+echo "Be sure to set your iTerm theme to Nord and font to Source Code Pro."
